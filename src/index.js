18959dbf4fd41a0d4bc8e499baa3720098f554a1
Eliot Sanford <eliot587@gmail.com>
2019-05-31 16:50:53 -0500

    commented out js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import "./styles.css";
+// import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use Parcel to bundle this sandbox,you can find more info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// document.getElementById("app").innerHTML = `
+// <h1>Hello Vanilla!</h1>
+// <div>
+//   We use Parcel to bundle this sandbox,you can find more info about Parcel
+//   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+// </div>
+// `;
